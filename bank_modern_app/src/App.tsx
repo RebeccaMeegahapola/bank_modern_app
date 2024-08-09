721af059d044f980d0d49d3a1e08935bc2d5f38a
RebeccaMeegahapola <rebeccameegahapola@gmail.com>
2024-08-09 08:50:18 +0530

    Initial commit

---
+++ bank_modern_app/src/App.tsx
@@ -0,0 +1,11 @@
+import styles from "./style";
+
+const App = () => (
+  <div className='bg-primary w-full overflow-hidden'>
+    <div className={`${styles.paddingX}`}>
+    </div>
+  </div>
+)
+
+
+export default App
---
+++ bank_modern_app/src/types/assets.d.ts
@@ -0,0 +1,5 @@
+declare module '../assets' {
+    const content: any; // Use 'any' type if you don't have specific type information
+    export default content;
+}
+  

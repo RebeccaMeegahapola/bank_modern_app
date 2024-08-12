4ec4fe5875765df8d1b0353d98318df1ef7c59c7
RebeccaMeegahapola <rebeccameegahapola@gmail.com>
2024-08-12 12:15:54 +0530

    Create CardDeal component and edit button component

--- bank_modern_app/src/components/Button.tsx
+++ bank_modern_app/src/components/Button.tsx
@@ -4,7 +4,7 @@ type ButtonProps = {
 
 const Button = ({ styles }: ButtonProps) => {
   return (
-    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}>
         Get Started
     </button>
   )
